e9e3c7623c3eb298549d463e6d936410fb1be592
Noah Li Anderson <nlanderson@wustl.edu>
2022-12-03 17:50:00 -0600

    Q10_initial

--- Assets/Code/Adventure/PlayerController.cs
+++ Assets/Code/Adventure/PlayerController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 namespace Adventure
 {
+    public enum Direction
+    {
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3
+    }
     public class PlayerController : MonoBehaviour
     {
         //Outlets
         Rigidbody2D _rigidbody;
         Animator _animator;
+        SpriteRenderer _spriteRenderer;
+        public Transform[] attackZones;
 
         //Configuration
         public KeyCode keyUp;
@@ -20,12 +29,17 @@ namespace Adventure
         public KeyCode keyRight;
 
         public float moveSpeed;
+        public Sprite[] sprites;
+        
+        //State Tracking
+        public Direction facingDirection;
 
         // Start is called before the first frame update
         void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         // Update is called once per frame
@@ -42,6 +56,25 @@ namespace Adventure
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _animator.SetTrigger("attack");
+                
+                //Convert the enumeration to an index
+                int facingDirectionIndex = (int)facingDirection;
+                
+                //Get an attack zone from index
+                Transform attackZone = attackZones[facingDirectionIndex];
+                
+                //What objects are within a circle at that attack zone?
+                Collider2D[] hits = Physics2D.OverlapCircleAll(attackZone.position, 0.1f);
+                
+                //Handle each hit target
+                foreach (Collider2D hit in hits)
+                {
+                    Breakable breakableObject = hit.GetComponent<Breakable>();
+                    if (breakableObject)
+                    {
+                        breakableObject.Break();
+                    }
+                }
             }
         }
 
@@ -65,6 +98,18 @@ namespace Adventure
             if (Input.GetKey(keyRight))
             {
                 _rigidbody.AddForce(Vector2.right * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
+        }
+
+        void LateUpdate()
+        {
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                if (_spriteRenderer.sprite == sprites[i])
+                {
+                    facingDirection = (Direction)i;
+                    break;
+                }
             }
         }
     }
